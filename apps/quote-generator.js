03765653fabd5a6324cd721a4e8e5402eae4c843
yamam1970 <yamamtawalbeh202@gmail.com>
2025-06-19 19:45:25 +0300

    Implemented Quote Generator App by Abd-AlGhani

--- apps/quote-generator.js
+++ apps/quote-generator.js
@@ -1,39 +1,34 @@
-// quote-generator.js 
+// quote-generator.js
 // Developed by Abdelghani Al-Ghoul
 
-const quotes = [
-    "The best way to get started is to quit talking and begin doing. - Walt Disney",
-    "Don't let yesterday take up too much of today. - Will Rogers",
-    "It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
-    "If you are working on something exciting, it will keep you motivated. - Unknown",
-    "Success is not in what you have, but who you are. - Bo Bennett",
+const quotes = [ // Array of quotes to pick from
+  "The best way to get started is to quit talking and begin doing. - Walt Disney",
+  "Don't let yesterday take up too much of today. - Will Rogers",
+  "It's not whether you get knocked down, it's whether you get up. - Vince Lombardi",
+  "If you are working on something exciting, it will keep you motivated. - Unknown",
+  "Success is not in what you have, but who you are. - Bo Bennett"
 ];
 
-function showApp(appName) {
-    AppUtils.hide('main-view');
-    AppUtils.hide('raid-app');
-    AppUtils.hide('quote-generator-app');
-
-    if (appName === 'quote-generator') {
-        AppUtils.show('quote-generator-app');
-    } else if (appName === 'raid') {
-        AppUtils.show('raid-app');
-    } else {
-        AppUtils.show('main-view');
-    }
+function showApp(appName) { // Show selected app and hide others
+  AppUtils.hide('main-view');
+  AppUtils.hide('raid-app');
+  AppUtils.hide('quote-generator-app');
+  if (appName === 'quote-generator') AppUtils.show('quote-generator-app');
+  else if (appName === 'raid') AppUtils.show('raid-app');
+  else AppUtils.show('main-view');
 }
 
-function showMain() {
-    AppUtils.hide('raid-app');
-    AppUtils.hide('quote-generator-app');
-    AppUtils.show('main-view');
+function showMain() { // Show main hub and hide apps
+  AppUtils.hide('raid-app');
+  AppUtils.hide('quote-generator-app');
+  AppUtils.show('main-view');
 }
 
-function generateQuote() {
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    document.getElementById('quote-container').innerText = quote;
+function generateQuote() { // Pick random quote and display it
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  document.getElementById('quote-container').innerText = quote;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    AppUtils.onClick('generate-quote', generateQuote);
-});
+document.addEventListener('DOMContentLoaded', () => // Bind button click after DOM loads
+  AppUtils.onClick('generate-quote', generateQuote)
+);
